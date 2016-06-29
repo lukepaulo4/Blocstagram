c1741175aac2f0d7d61223d02495db5f61c5557d
Luke Paulo <lukepaulo4@gmail.com>
2016-06-29 00:18:25 -0700

    Checkpoint 27 interfaces: completed

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Luke Paulo on 6/28/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@end
