3169830954d70bc12f6ce390d36b635b103bf42a
Luke Paulo <lukepaulo4@gmail.com>
2016-06-28 22:46:53 -0700

    task one in the checkpoint

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Luke Paulo on 6/28/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
